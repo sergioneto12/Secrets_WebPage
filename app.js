62505715297ba0c45fa73d134769e2e65adeda6f
sergioneto12 <sergio.ght14@gmail.com>
2021-05-01 14:48:31 -0300

    security tasks

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+var encrypt = require('mongoose-encryption');
 
 const app = express();
+console.log(process.env.API_KEY)
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -14,10 +17,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true})
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-}
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema)
 
